e493ca4a0de365f4b5cc1c58e106209e3dbdc0df
Tony Tam <fehguy@gmail.com>
2012-10-06 14:37:57 -0700

    changed paths

--- bin/server.sh
+++ bin/server.sh
@@ -1,4 +1,4 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 export CLASSPATH="$DIR/../target/lib/*:$DIR/../target/*"
 export JAVA_OPTS="${JAVA_OPTS} -DXmx4096M"
-scala $WORDNIK_OPTS -cp $CLASSPATH "$@" src/main/scripts/NodeFromServer.scala -DfileMap=src/main/html/api
+scala $WORDNIK_OPTS -cp $CLASSPATH "$@" src/main/scripts/ScalatraServerGenerator.scala -DfileMap=src/main/templates/spec-files
