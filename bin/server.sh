1776c1407b6fe37f29cdd52b6043d5c4483b1011
Tony Tam <fehguy@gmail.com>
2012-10-07 09:46:39 -0700

    fixed allowable values for new scalatra interface

--- bin/server.sh
+++ bin/server.sh
@@ -1,4 +1,18 @@
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-export CLASSPATH="$DIR/../target/lib/*:$DIR/../target/*"
+#!/bin/bash
+echo "" > classpath.txt
+for file in `ls target/lib/*.jar`;
+        do echo -n '' >> classpath.txt;
+        echo -n $file >> classpath.txt;
+        echo -n ':' >> classpath.txt;
+done
+for file in `ls target/*.jar`;
+        do echo -n '' >> classpath.txt;
+        echo -n $file >> classpath.txt;
+        echo -n ':' >> classpath.txt;
+done
+export CLASSPATH=$(cat classpath.txt)
+
+#DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+#export CLASSPATH="$DIR/../target/lib/*:$DIR/../target/*"
 export JAVA_OPTS="${JAVA_OPTS} -DXmx4096M"
 scala $WORDNIK_OPTS -cp $CLASSPATH "$@" src/main/scripts/ScalatraServerGenerator.scala -DfileMap=src/main/templates/spec-files
--- src/main/scripts/ScalatraServerGenerator.scala
+++ src/main/scripts/ScalatraServerGenerator.scala
@@ -31,40 +31,22 @@ object ScalatraServerGenerator extends BasicScalaGenerator {
     ("README.mustache",               outputFolder,                                 "README.md"),
     ("build.sbt",                     outputFolder,                                 "build.sbt"),
 
-    ("project/build.properties",      outputFolder + "/project",                     "build.properties"),
-    ("project/plugins.sbt",           outputFolder + "/project",                     "plugins.sbt"),
+    ("project",                       outputFolder + "/project",                     ""),
 
     ("JettyMain.scala",               outputFolder + "/src/main/scala",             "JettyMain.scala"),
     ("web.xml",                       outputFolder + "/target/webapp/WEB-INF",      "web.xml"),    
-    ("admin/index.html",              outputFolder + "/target/webapp/admin",        "index.html"),
-    ("admin/swagger-ui.js",           outputFolder + "/target/webapp/admin",        "swagger-ui.js"),
-    ("admin/swagger-ui.min.js",       outputFolder + "/target/webapp/admin",        "swagger-ui.min.js"),
+    ("admin",                         outputFolder + "/target/webapp/admin",        ""))
 
-    ("admin/css/screen.css",          outputFolder + "/target/webapp/admin/css",    "screen.css"),
-
-    ("admin/images/logo_small.png",   outputFolder + "/target/webapp/admin/images", "logo_small.png"),
-    ("admin/images/pet_store_api.png",outputFolder + "/target/webapp/admin/images", "pet_store_api.png"),
-    ("admin/images/throbber.gif",     outputFolder + "/target/webapp/admin/images", "throbber.gif"),
-    ("admin/images/wordnik_api.png",  outputFolder + "/target/webapp/admin/images", "wordnik_api.png"),
-
-    ("admin/lib/backbone-min.js",     outputFolder + "/target/webapp/admin/lib",    "backbone-min.js"),
-    ("admin/lib/handlebars.runtime-1.0.0.beta.6.js", 
-                                      outputFolder + "/target/webapp/admin/lib",    "handlebars.runtime-1.0.0.beta.6.js"),
-    ("admin/lib/jquery-1.8.0.min.js", outputFolder + "/target/webapp/admin/lib",    "jquery-1.8.0.min.js"),
-    ("admin/lib/jquery.ba-bbq.min.js",outputFolder + "/target/webapp/admin/lib",    "jquery.ba-bbq.min.js"),
-    ("admin/lib/jquery.slideto.min.js",
-                                      outputFolder + "/target/webapp/admin/lib",    "jquery.slideto.min.js"),
-    ("admin/lib/jquery.wiggle.min.js",outputFolder + "/target/webapp/admin/lib",    "jquery.wiggle.min.js"),
-    ("admin/lib/swagger.js",          outputFolder + "/target/webapp/admin/lib",    "swagger.js"),
-    ("admin/lib/underscore-min.js",   outputFolder + "/target/webapp/admin/lib",    "underscore-min.js")
-    )
-
+  /**
+   * quoting all as strings
+   **/
   override def toDefaultValue(datatype: String, v: String): Option[String] = {
     if (v != "" && v != null) {
       datatype match {
         case "int" => Some("\"" + v + "\"")
         case "long" => Some("\"" + v + "\"")
         case "double" => Some("\"" + v + "\"")
+        case "boolean" => Some("\"" + v + "\"")
         case x if x == "string" || x == "String" => {
           v match {
             case e: String => Some("\"" + v + "\"")
@@ -82,11 +64,47 @@ object ScalatraServerGenerator extends BasicScalaGenerator {
 
     mutable.map(k => {
       k._1 match {
+        case "allParams" => {
+          val paramList = k._2.asInstanceOf[List[_]]
+          paramList.foreach(param => {
+            val map = param.asInstanceOf[scala.collection.mutable.HashMap[String, AnyRef]]
+            if(map.contains("allowableValues")) {
+              val allowableValues = map("allowableValues")
+              println(map("swaggerDataType") + ", " + allowableValues)
+              val quote = map("swaggerDataType") match {
+                case "string" => "\""
+                case _ => ""
+              }
+              val pattern = "([A-Z]*)\\[(.*)\\]".r
+              val str = allowableValues match {
+                case pattern(valueType, values) => {
+                  valueType match {
+                    case "LIST" => {
+                      val l = values.split(",").toList
+                      Some("AllowableValues(" + l.mkString(quote, quote + "," + quote, quote + ")"))
+                    }
+                    case "RANGE" => {
+                      val r = values.split(",")
+                      Some("AllowableValues(Range(" + r(0) + "," + r(1) + ", 1))")
+                    }
+                  }
+                }
+                case _ => None
+              }
+              str match {
+                case Some(s) => map += "allowableValues" -> s
+                case _ =>
+              }
+              println("default value: " + map.getOrElse("defaultValue", "not found!"))
+            }
+          })
+        }
+
         // the scalatra templates like lower-case httpMethods
-        case e: String if (e == "httpMethod") => mutable += "httpMethod" -> k._2.toString.toLowerCase
+        case "httpMethod" => mutable += "httpMethod" -> k._2.toString.toLowerCase
 
         // convert path into ruby-ish syntax without basePart (i.e. /pet.{format}/{petId} => /:petId
-        case e: String if (e == "path") => {
+        case "path" => {
           val path = {
             val arr = k._2.toString.split("/")
             if (arr.length >= 2) {
@@ -96,7 +114,6 @@ object ScalatraServerGenerator extends BasicScalaGenerator {
               k._2.toString
           }
           // rip out the root path
-
           mutable += "path" -> path.replaceAll("\\{", ":").replaceAll("\\}", "")
         }
         case _ =>
