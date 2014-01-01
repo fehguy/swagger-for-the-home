674966391b09eda2d082a203334fb0ee036137e7
Tony Tam <fehguy@gmail.com>
2013-12-31 16:58:43 -0800

    updated script

--- bin/server.sh
+++ bin/server.sh
@@ -1,17 +1,1 @@
-#!/bin/bash
-echo "" > classpath.txt
-for file in `ls target/lib/*.jar`;
-        do echo -n '' >> classpath.txt;
-        echo -n $file >> classpath.txt;
-        echo -n ':' >> classpath.txt;
-done
-for file in `ls target/*.jar`;
-        do echo -n '' >> classpath.txt;
-        echo -n $file >> classpath.txt;
-        echo -n ':' >> classpath.txt;
-done
-export CLASSPATH=$(cat classpath.txt)
-export JAVA_OPTS="${JAVA_OPTS} -DXmx4096M"
-
-scala $WORDNIK_OPTS -cp $CLASSPATH "$@" src/main/scripts/ScalatraServerGenerator.scala -DfileMap=src/main/templates/spec-files
-cp -r src/main/templates/scalatra/admin server/src/main/webapp/admin
+scala -cp swagger-codegen.jar -DfileMap=src/main/specs/api-docs src/main/scala/ScalatraServerGenerator.scala http://localhost:8080
