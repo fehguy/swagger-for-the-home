b12542a726060a3ac1d8093f7505031b8ce4a5e7
Tony Tam <fehguy@gmail.com>
2014-11-16 15:30:09 -0800

    updated client

--- ios/SFTHClient/SFTHClient/SecondViewController.h
+++ ios/SFTHClient/SFTHClient/SecondViewController.h
@@ -1,15 +1,62 @@
-//
-//  SecondViewController.h
-//  SFTHClient
-//
-//  Created by Tony Tam on 11/1/14.
-//  Copyright (c) 2014 Eatbacon. All rights reserved.
-//
-
 #import <UIKit/UIKit.h>
+#import "SWGPhidgetApi.h"
 
-@interface SecondViewController : UIViewController
+@interface SecondViewController : UIViewController {
+    UISwitch* switch1;
+    UISwitch* switch2;
+    UISwitch* switch3;
+    UISwitch* switch4;
+    UISwitch* switch5;
+    UISwitch* switch6;
+    UISwitch* switch7;
+    UISwitch* switch8;
+    UISwitch* switch9;
+    UISwitch* switch10;
 
+}
+
+-(IBAction)switch1ValueChanged:(id)sender;
+-(IBAction)switch1Timer:(id)sender;
+
+-(IBAction)switch2ValueChanged:(id)sender;
+-(IBAction)switch2Timer:(id)sender;
+
+-(IBAction)switch3ValueChanged:(id)sender;
+-(IBAction)switch3Timer:(id)sender;
+
+-(IBAction)switch4ValueChanged:(id)sender;
+-(IBAction)switch4Timer:(id)sender;
+
+-(IBAction)switch5ValueChanged:(id)sender;
+-(IBAction)switch5Timer:(id)sender;
+
+-(IBAction)switch6ValueChanged:(id)sender;
+-(IBAction)switch6Timer:(id)sender;
+
+-(IBAction)switch7ValueChanged:(id)sender;
+-(IBAction)switch7Timer:(id)sender;
+
+-(IBAction)switch8ValueChanged:(id)sender;
+-(IBAction)switch8Timer:(id)sender;
+
+-(IBAction)switch9ValueChanged:(id)sender;
+-(IBAction)switch9Timer:(id)sender;
+
+-(IBAction)switch10ValueChanged:(id)sender;
+-(IBAction)switch10Timer:(id)sender;
+
+-(IBAction)refreshState;
+
+@property (nonatomic, retain) IBOutlet UISwitch* switch1;
+@property (nonatomic, retain) IBOutlet UISwitch* switch2;
+@property (nonatomic, retain) IBOutlet UISwitch* switch3;
+@property (nonatomic, retain) IBOutlet UISwitch* switch4;
+@property (nonatomic, retain) IBOutlet UISwitch* switch5;
+@property (nonatomic, retain) IBOutlet UISwitch* switch6;
+@property (nonatomic, retain) IBOutlet UISwitch* switch7;
+@property (nonatomic, retain) IBOutlet UISwitch* switch8;
+@property (nonatomic, retain) IBOutlet UISwitch* switch9;
+@property (nonatomic, retain) IBOutlet UISwitch* switch10;
 
 @end
 
--- ios/SFTHClient/SFTHClient/SecondViewController.m
+++ ios/SFTHClient/SFTHClient/SecondViewController.m
@@ -16,7 +16,292 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    [self refreshState];
     // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)refreshState {
+    SWGPhidgetApi * api = [[SWGPhidgetApi alloc] init];
+
+    [api getRelayOutputWithCompletionBlock:@4 completionHandler:^(SWGDigitalIO *output, NSError *error) {
+        if(output) {
+            if([[output value] isEqualToNumber:[[NSNumber alloc] initWithBool:TRUE]])
+                [self.switch1 setOn:YES animated:YES];
+            else
+                [self.switch1 setOn:NO animated:NO];
+        }
+    }];
+    
+    [api getRelayOutputWithCompletionBlock:@6 completionHandler:^(SWGDigitalIO *output, NSError *error) {
+        if(output) {
+            if([[output value] isEqualToNumber:[[NSNumber alloc] initWithBool:TRUE]])
+                [self.switch2 setOn:YES animated:YES];
+            else
+                [self.switch2 setOn:NO animated:NO];
+        }
+    }];
+    
+    [api getRelayOutputWithCompletionBlock:@7 completionHandler:^(SWGDigitalIO *output, NSError *error) {
+        if(output) {
+            if([[output value] isEqualToNumber:[[NSNumber alloc] initWithBool:TRUE]])
+                [self.switch3 setOn:YES animated:YES];
+            else
+                [self.switch3 setOn:NO animated:NO];
+        }
+    }];
+    
+    [api getRelayOutputWithCompletionBlock:@9 completionHandler:^(SWGDigitalIO *output, NSError *error) {
+        if(output) {
+            if([[output value] isEqualToNumber:[[NSNumber alloc] initWithBool:TRUE]])
+                [self.switch4 setOn:YES animated:YES];
+            else
+                [self.switch4 setOn:NO animated:NO];
+        }
+    }];
+    
+    [api getRelayOutputWithCompletionBlock:@12 completionHandler:^(SWGDigitalIO *output, NSError *error) {
+        if(output) {
+            if([[output value] isEqualToNumber:[[NSNumber alloc] initWithBool:TRUE]])
+                [self.switch5 setOn:YES animated:YES];
+            else
+                [self.switch5 setOn:NO animated:NO];
+        }
+    }];
+    
+    [api getRelayOutputWithCompletionBlock:@10 completionHandler:^(SWGDigitalIO *output, NSError *error) {
+        if(output) {
+            if([[output value] isEqualToNumber:[[NSNumber alloc] initWithBool:TRUE]])
+                [self.switch6 setOn:YES animated:YES];
+            else
+                [self.switch6 setOn:NO animated:NO];
+        }
+    }];
+    
+    
+    [api getRelayOutputWithCompletionBlock:@11 completionHandler:^(SWGDigitalIO *output, NSError *error) {
+        if(output) {
+            if([[output value] isEqualToNumber:[[NSNumber alloc] initWithBool:TRUE]])
+                [self.switch7 setOn:YES animated:YES];
+            else
+                [self.switch7 setOn:NO animated:NO];
+        }
+    }];
+    
+    [api getRelayOutputWithCompletionBlock:@3 completionHandler:^(SWGDigitalIO *output, NSError *error) {
+        if(output) {
+            if([[output value] isEqualToNumber:[[NSNumber alloc] initWithBool:TRUE]])
+                [self.switch8 setOn:YES animated:YES];
+            else
+                [self.switch8 setOn:NO animated:NO];
+        }
+    }];
+    
+    [api getRelayOutputWithCompletionBlock:@2 completionHandler:^(SWGDigitalIO *output, NSError *error) {
+        if(output) {
+            if([[output value] isEqualToNumber:[[NSNumber alloc] initWithBool:TRUE]])
+                [self.switch9 setOn:YES animated:YES];
+            else
+                [self.switch9 setOn:NO animated:NO];
+        }
+    }];
+    
+    [api getRelayOutputWithCompletionBlock:@8 completionHandler:^(SWGDigitalIO *output, NSError *error) {
+        if(output) {
+            if([[output value] isEqualToNumber:[[NSNumber alloc] initWithBool:TRUE]])
+                [self.switch10 setOn:YES animated:YES];
+            else
+                [self.switch10 setOn:NO animated:NO];
+        }
+    }];
+}
+
+-(IBAction)switch1ValueChanged:(id)sender {
+    SWGPhidgetApi * api = [[SWGPhidgetApi alloc] init];
+    
+    UISwitch* l_switch = (UISwitch*) sender;
+    
+    BOOL state = FALSE;
+    if([l_switch isOn]) {
+        state = TRUE;
+    }
+    
+    [api setOutputRelayWithCompletionBlock:[[NSNumber alloc] initWithBool:state] position:@4
+                         completionHandler:^(SWGDigitalIO *output, NSError *error) {
+                             if(output) {
+                                 NSLog(@"ok!");
+                             }
+                         }];
+}
+
+
+-(IBAction)switch2ValueChanged:(id)sender {
+    SWGPhidgetApi * api = [[SWGPhidgetApi alloc] init];
+    
+    UISwitch* l_switch = (UISwitch*) sender;
+    
+    BOOL state = FALSE;
+    if([l_switch isOn]) {
+        state = TRUE;
+    }
+    
+    [api setOutputRelayWithCompletionBlock:[[NSNumber alloc] initWithBool:state] position:@6
+                         completionHandler:^(SWGDigitalIO *output, NSError *error) {
+                             if(output) {
+                                 NSLog(@"ok!");
+                             }
+                         }];
+}
+
+
+-(IBAction)switch3ValueChanged:(id)sender {
+    SWGPhidgetApi * api = [[SWGPhidgetApi alloc] init];
+    
+    UISwitch* l_switch = (UISwitch*) sender;
+    
+    BOOL state = FALSE;
+    if([l_switch isOn]) {
+        state = TRUE;
+    }
+    
+    [api setOutputRelayWithCompletionBlock:[[NSNumber alloc] initWithBool:state] position:@7
+                         completionHandler:^(SWGDigitalIO *output, NSError *error) {
+                             if(output) {
+                                 NSLog(@"ok!");
+                             }
+                         }];
+}
+
+
+-(IBAction)switch4ValueChanged:(id)sender {
+    SWGPhidgetApi * api = [[SWGPhidgetApi alloc] init];
+    
+    UISwitch* l_switch = (UISwitch*) sender;
+    
+    BOOL state = FALSE;
+    if([l_switch isOn]) {
+        state = TRUE;
+    }
+    
+    [api setOutputRelayWithCompletionBlock:[[NSNumber alloc] initWithBool:state] position:@9
+                         completionHandler:^(SWGDigitalIO *output, NSError *error) {
+                             if(output) {
+                                 NSLog(@"ok!");
+                             }
+                         }];
+}
+
+
+-(IBAction)switch5ValueChanged:(id)sender {
+    SWGPhidgetApi * api = [[SWGPhidgetApi alloc] init];
+    
+    UISwitch* l_switch = (UISwitch*) sender;
+    
+    BOOL state = FALSE;
+    if([l_switch isOn]) {
+        state = TRUE;
+    }
+    
+    [api setOutputRelayWithCompletionBlock:[[NSNumber alloc] initWithBool:state] position:@12
+                         completionHandler:^(SWGDigitalIO *output, NSError *error) {
+                             if(output) {
+                                 NSLog(@"ok!");
+                             }
+                         }];
+}
+
+
+-(IBAction)switch6ValueChanged:(id)sender {
+    SWGPhidgetApi * api = [[SWGPhidgetApi alloc] init];
+    
+    UISwitch* l_switch = (UISwitch*) sender;
+    
+    BOOL state = FALSE;
+    if([l_switch isOn]) {
+        state = TRUE;
+    }
+    
+    [api setOutputRelayWithCompletionBlock:[[NSNumber alloc] initWithBool:state] position:@10
+                         completionHandler:^(SWGDigitalIO *output, NSError *error) {
+                             if(output) {
+                                 NSLog(@"ok!");
+                             }
+                         }];
+}
+
+
+-(IBAction)switch7ValueChanged:(id)sender {
+    SWGPhidgetApi * api = [[SWGPhidgetApi alloc] init];
+    
+    UISwitch* l_switch = (UISwitch*) sender;
+    
+    BOOL state = FALSE;
+    if([l_switch isOn]) {
+        state = TRUE;
+    }
+    
+    [api setOutputRelayWithCompletionBlock:[[NSNumber alloc] initWithBool:state] position:@11
+                         completionHandler:^(SWGDigitalIO *output, NSError *error) {
+                             if(output) {
+                                 NSLog(@"ok!");
+                             }
+                         }];
+}
+
+
+-(IBAction)switch8ValueChanged:(id)sender {
+    SWGPhidgetApi * api = [[SWGPhidgetApi alloc] init];
+    
+    UISwitch* l_switch = (UISwitch*) sender;
+    
+    BOOL state = FALSE;
+    if([l_switch isOn]) {
+        state = TRUE;
+    }
+    
+    [api setOutputRelayWithCompletionBlock:[[NSNumber alloc] initWithBool:state] position:@1
+                         completionHandler:^(SWGDigitalIO *output, NSError *error) {
+                             if(output) {
+                                 NSLog(@"ok!");
+                             }
+                         }];
+}
+
+
+-(IBAction)switch9ValueChanged:(id)sender {
+    SWGPhidgetApi * api = [[SWGPhidgetApi alloc] init];
+    
+    UISwitch* l_switch = (UISwitch*) sender;
+    
+    BOOL state = FALSE;
+    if([l_switch isOn]) {
+        state = TRUE;
+    }
+    
+    [api setOutputRelayWithCompletionBlock:[[NSNumber alloc] initWithBool:state] position:@3
+                         completionHandler:^(SWGDigitalIO *output, NSError *error) {
+                             if(output) {
+                                 NSLog(@"ok!");
+                             }
+                         }];
+}
+
+
+-(IBAction)switch10ValueChanged:(id)sender {
+    SWGPhidgetApi * api = [[SWGPhidgetApi alloc] init];
+    
+    UISwitch* l_switch = (UISwitch*) sender;
+    
+    BOOL state = FALSE;
+    if([l_switch isOn]) {
+        state = TRUE;
+    }
+    
+    [api setOutputRelayWithCompletionBlock:[[NSNumber alloc] initWithBool:state] position:@2
+                         completionHandler:^(SWGDigitalIO *output, NSError *error) {
+                             if(output) {
+                                 NSLog(@"ok!");
+                             }
+                         }];
 }
 
 - (void)didReceiveMemoryWarning {
